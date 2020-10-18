257faae0c06176659b60a1d028f169accc7911fe
miqbalrr <iqb4lcakep@gmail.com>
2020-10-19 00:29:06 +0700

    adding tera template

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{error, web, Error, App, HttpResponse, HttpServer};
 use listenfd::ListenFd;
+
+use tera::Tera;
 
 #[actix_web::main]
 pub async fn start() -> std::io::Result<()> {
+    env_logger::init();
     let mut listenfd = ListenFd::from_env();
     let mut server =  HttpServer::new(|| {
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/views/**/*")).unwrap();
+
         App::new()
-        .route("/", web::get().to(index))
+        .data(tera)
+        .service(web::resource("/").route(web::get().to(index)))
     });
 
     server = match listenfd.take_tcp_listener(0).unwrap() {
@@ -17,6 +23,14 @@ pub async fn start() -> std::io::Result<()> {
     server.run().await
 }
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Iqbalcakep.com is running :) ")
+async fn index(
+    template: web::Data<tera::Tera>
+) -> Result<HttpResponse, Error> {
+
+    let mut ctx = tera::Context::new();
+    ctx.insert("greet", &"Welcome to Iqbalcakep".to_owned());
+    let body = template.render("index.html", &ctx)
+        .map_err(|_| error::ErrorInternalServerError("Template Error"))?;
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(body))
 }
